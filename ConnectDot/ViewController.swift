3f3aa28d283d86a58a674da53c4354c89e4f2805
RemTod <jremy994@gmail.com>
2019-10-07 11:06:06 +0700

    Initial Commit

---
+++ ConnectDot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConnectDot
+//
+//  Created by Rem Remy on 07/10/19.
+//  Copyright © 2019 Rem Remy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
